7c6bc76fde9938f3cb8d8a38b9c167fc224b300e
Kaspar-PotBS <94735202+Kaspar-PotBS@users.noreply.github.com>
2021-12-12 16:13:51 +0100

    Add files via upload

---
+++ Compare2/js/highlight.js
@@ -0,0 +1,84 @@
+jQuery(function($) {
+	$(".filter").click(function() {
+	
+var master = [];
+
+// Identify the master values.
+
+$('.selected ul' ).first().find('li').each(function(index,value) {
+    master.push($(this).text());
+});
+
+
+$('ul:not(:first)').each(function(ulIndex) {
+    $(this).find('li.higherisbetter').each(function(index) {
+        if(master[index] > Number($(this).text())) {
+			$(this).addClass('compareworse');			
+        } 		
+    });
+});
+
+
+$('ul:not(:first)').each(function(ulIndex) {
+    $(this).find('li.higherisbetter').each(function(index) {
+        if(master[index] < Number($(this).text())) {
+			$(this).addClass('comparebetter');			
+        } 	
+    });
+});
+
+
+$('ul:not(:first)').each(function(ulIndex) {
+    $(this).find('.lowerisbetter li').each(function(index) {
+        if(master[index] > Number($(this).text())) {
+			$(this).addClass('comparebetter');			
+        } 		
+    });
+});
+
+
+$('ul:not(:first)').each(function(ulIndex) {
+    $(this).find('.lowerisbetter li').each(function(index) {
+        if(master[index] < Number($(this).text())) {
+			$(this).addClass('compareworse');			
+        } 	
+    });
+});
+
+
+
+$('ul:not(:first)').each(function(ulIndex) {
+    $(this).find('li').each(function(index) {
+        if(master[index] == Number($(this).text())) {
+            $(this).addClass('comparematch');        
+        }  
+    });
+});
+
+
+});
+	
+
+
+
+
+	$(".reset").click(function() {
+	
+var master = [];
+
+// Identify the master values.
+
+$('.selected ul' ).first().find('li').each(function(index,value) {
+    master.push($(this).text());
+});
+
+$('ul:not(:first)').each(function(ulIndex) {
+    $(this).find('li').each(function(index) {
+            $(this).removeClass('comparebetter');
+			$(this).removeClass('compareworse');
+			$(this).removeClass('comparematch');
+    });
+});
+});
+
+});
