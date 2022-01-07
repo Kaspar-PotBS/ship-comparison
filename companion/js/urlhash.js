f1add70fcc5409e4c0e9cb3101cb3a8471a0fe5c
Kaspar-PotBS <94735202+Kaspar-PotBS@users.noreply.github.com>
2022-01-07 07:13:02 +0100

    Add files via upload

---
+++ companion/js/urlhash.js
@@ -0,0 +1,38 @@
+
+
+$(document).ready(function () {
+		
+var hashParams = window.location.hash.substr(1).split('&'); // substr(1) to remove the `#`
+for(var i = 0; i < hashParams.length; i++){
+    var p = hashParams[i].split('=');
+	console.log("hash val " + p[1]);
+    document.getElementById(p[0]).value = decodeURIComponent(p[1]);;
+	
+	StatsLookupA(p[1]);
+	$('#ship').on('change', function(){
+
+    });
+	$('#ship').change();
+	
+	
+	
+};
+});
+
+
+
+//$(function(){
+//  $('#ship').change(function () {
+//       var selnum = $(this).val();
+//		var url = "ship="+selnum;
+//        window.location.hash = url;
+//    });
+//});
+//window.addEventListener('hashchange', fn, false);
+//
+//window.onload = fn; // fire on pageload
+//
+//function fn() {
+//   $('#ship').val(window.location.hash.replace('#', ''));
+//	
+//}
